8e1070bcc1adf6e18b1c6a6b3840e551c7ce5665
Oddmund Strømme <oddmund@oddmundo.com>
2010-11-07 23:05:09 +0100

    moved init stuff into init

--- lib/svirfneblin.rb
+++ lib/svirfneblin.rb
@@ -1,32 +1,34 @@
 # -*- coding: utf-8 -*-
 require 'luck'
-require 'luck/canvas'
 
 class Svirfneblin
+  def initialize
+    @display = Luck::Display.new nil
+    @pane = Luck::Pane.new @display, 0, 0, @display.width, @display.height, "SVIRFNEBLIN"
+    @display.pane :main, @pane
+    @pane.control :labia, Luck::Label, 2, 2, @pane.width, @pane.height
+
+    @level = ""
+    @level << ".........................................\n"
+    @level << "...............╱.........................\n"
+    @level << "...............│..........................\n"
+    @level << ".......┌───────╯──┐......................\n"
+    @level << ".......│          │......................\n"
+    @level << ".......│    @     ┊......................\n"
+    @level << ".......│          ┊......................\n"
+    @level << ".......╘══════════╛......................\n"
+    @level << ".........................................\n"
+    @level << ".........................................\n"
+
+    @pane.controls[:labia].text = @level
+  end
+
   def run
     begin
-      d = Luck::Display.new nil
-      p = Luck::Pane.new d, 0,0, d.width, d.height, "SVIRFNEBLIN"
-      d.pane :main, p
-
-      p.control :labia, Luck::Label, 2, 2, p.width, p.height do
-        @text = ""
-        @text << ".........................................\n"
-        @text << "...............╱.........................\n"
-        @text << "...............│..........................\n"
-        @text << ".......┌───────╯──┐......................\n"
-        @text << ".......│          │......................\n"
-        @text << ".......│    @     ┊......................\n"
-        @text << ".......│          ┊......................\n"
-        @text << ".......╘══════════╛......................\n"
-        @text << ".........................................\n"
-        @text << ".........................................\n"
-      end
-
-      d.handle
+      @display.handle
       gets
     rescue => ex
-      d.close
+      @display.close
       p ex.class, ex.message, ex.backtrace
     end
   end
