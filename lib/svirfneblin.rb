0e37159848c5896758493783beb4f793b6c4145f
Oddmund Strømme <oddmund@oddmundo.com>
2010-11-13 15:57:44 +0100

    closing in on something that works.

--- lib/svirfneblin.rb
+++ lib/svirfneblin.rb
@@ -1,35 +1,56 @@
-# -*- coding: utf-8 -*-
 require 'luck'
+
+@@WIDTH = 80
+@@HEIGHT = 24
+
+class Coordinate
+  def initialize(x,y)
+    @x,@y = x,y
+  end
+
+  def x
+    @x
+  end
+
+  def y
+    @y
+  end
+end
 
 class Svirfneblin
   def initialize
     @display = Luck::Display.new nil
-    @pane = Luck::Pane.new @display, 0, 0, @display.width, @display.height, "SVIRFNEBLIN"
-    @display.pane :main, @pane
-    @pane.control :labia, Luck::Label, 2, 2, @pane.width, @pane.height
-
-    @level = ""
-    @level << ".........................................\n"
-    @level << "...............╱.........................\n"
-    @level << "...............│..........................\n"
-    @level << ".......┌───────╯──┐......................\n"
-    @level << ".......│          │......................\n"
-    @level << ".......│    @     ┊......................\n"
-    @level << ".......│          ┊......................\n"
-    @level << ".......╘══════════╛......................\n"
-    @level << ".........................................\n"
-    @level << ".........................................\n"
-
-    @pane.controls[:labia].text = @level
+    @map = Array.new(@@WIDTH).map! { Array.new(@@HEIGHT) }
+    @hero = Coordinate.new(5,5)
+      @exit = false
   end
 
   def run
     begin
-      @display.handle
-      gets
+      until @exit == true do
+        draw
+        handle
+      end
+      quit
     rescue => ex
       @display.close
       p ex.class, ex.message, ex.backtrace
     end
+  end
+
+  def draw
+    @display.redraw
+    (0..20).each do |y|
+      (0..70).each do |x|
+        if @map[x][y] != nil
+          @display.place x, y, @map[x][y].to_s
+        end
+      end
+    end
+    @display.place @hero.x, @hero.y, "@"
+  end
+
+  def handle
+    c = $<.getc
   end
 end
