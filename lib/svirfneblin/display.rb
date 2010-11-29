163b7b0de4a0ca8463bae5d3047c9eaf9dfcc4c8
Oddmund Strømme <oddmund@oddmundo.com>
2010-11-29 13:49:00 +0100

    Ncurses works

--- lib/svirfneblin/display.rb
+++ lib/svirfneblin/display.rb
@@ -1,12 +1,18 @@
 require 'luck'
+require 'ncursesw'
 
 class Display
   def initialize adapter=:ncurses
     @adapter = LuckAdapter.new if adapter==:luck
+    @adapter = NcurseswAdapter.new if adapter==:ncursesw
   end
   
   def close
     @adapter.close    
+  end
+
+  def getc
+    @adapter.getc
   end
 
   def redraw
@@ -15,6 +21,10 @@ class Display
 
   def place x, y, tile
     @adapter.place x, y, tile
+  end
+
+  def clear
+    @adapter.clear
   end
 end
 
@@ -32,7 +42,35 @@ class LuckAdapter
   end
 
   def place x, y, tile
-    @d.place x, y, tile
+    @d.place y, x, tile
+  end
+end
+
+class NcurseswAdapter
+  def initialize
+    Ncurses.initscr
+    Ncurses.cbreak
+    Ncurses.noecho
   end
 
+  def clear
+    Ncurses.clear
+  end
+
+  def close
+    Ncurses.endwin
+  end
+
+  def redraw
+    Ncurses.refresh
+    
+  end
+
+  def place x, y, tile
+    Ncurses.mvaddstr y, x, tile.to_s
+  end
+
+  def getc
+    c = Ncurses.stdscr.getch.chr
+  end
 end
